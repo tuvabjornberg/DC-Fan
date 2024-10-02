82038d7e19136a5e350831ca9b72864e7a506537
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-10-02 11:20:55 +0200

    temp sensor prints

--- main/main.ino
+++ main/main.ino
@@ -9,10 +9,18 @@ void loop() {
   //Serial.print(voltage);
   //Serial.println(" Volts");
 
-  float temp = rawVoltage * (5.0 / 1023.0) * 2 + 18;
+  //float temp = rawVoltage * (5.0 / 1023.0) * 2 + 18;
+  //Serial.print(temp);
+  //Serial.println(" grader");
 
-  Serial.print(temp);
-  Serial.println(" grader");
+  float voltage = rawVoltage * (5.0 / 1023.0);
+  float temp_sensor = (voltage - 0.424)/ 0.00625; //OBS! Be wary of bottow view of temp sensor
+  
+  Serial.print(voltage);
+  Serial.println(" volt");
+  
+  Serial.print(temp_sensor);
+  Serial.println(" grader nu!");
 
-  delay(100);
+  delay(500);
 }
