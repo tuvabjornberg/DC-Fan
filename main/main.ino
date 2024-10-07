8411a5bc8d8240dc3ed867143e0ab4e8747376a7
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-10-07 16:59:31 +0200

    new names

--- main/main.ino
+++ main/main.ino
@@ -1,26 +1,33 @@
+int redLed = 4;
+int yellowLed = 3;
+int greenLed = 2;
+
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
+	pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
 }
 
 void loop() {
-  int rawVoltagePotent = analogRead(A0);
-  int rawVoltageSensor = analogRead(A1);
+  //int rawVoltagePotent = analogRead(A0);
+  int setpointVoltage = analogRead(A0);
+  int rawVoltageTmpSensor = analogRead(A1);
+
+  digitalWrite(redLed, HIGH);
+  delay(1000);                     
+  digitalWrite(redLed, LOW);
   
   //float voltage = rawValue * (5.0 / 1023.0);
-  //Serial.print(voltage);
-  //Serial.println(" Volts");
+  float setTemp = setpointVoltage * (5.0 / 1023.0) * 2 + 18; //Degrees celsius, set through pot. meter
 
-  //float temp = rawVoltagePotent * (5.0 / 1023.0) * 2 + 18;
-  //Serial.print(temp);
-  //Serial.println(" grader");
-
-  float voltage = rawVoltageSensor * (5.0 / 1023.0);
-  float temp_sensor = (voltage - 0.424)/ 0.00625; //OBS! Be wary of bottow view of temp sensor
+  float voltageOutTmpSensor = rawVoltageSensor * (5.0 / 1023.0);
+  float currentTempSensor = (voltageOutTmpSensor - 0.424)/ 0.00625; //OBS! Be wary of bottow view of temp sensor, function from tmp sensor datasheet
   
-  Serial.print(voltage);
+  Serial.print(voltageOutTmpSensor);
   Serial.println(" volt");
 
-  Serial.print(temp_sensor);
+  Serial.print(currentTempSensor);
   Serial.println(" grader nu!");
 
   delay(500);
