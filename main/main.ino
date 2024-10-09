bff4aa0906145068519c64957511a9865cb85df9
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-10-09 12:13:43 +0200

    some cleaning

--- main/main.ino
+++ main/main.ino
@@ -12,26 +12,13 @@ void setup() {
 }
 
 void loop() {
-  //int rawVoltagePotent = analogRead(A0);
   int setpointVoltage = analogRead(A0);
   int rawVoltageTmpSensor = analogRead(A1);
 
-  //digitalWrite(redLed, HIGH);
-  //delay(1000);                     
-  //digitalWrite(redLed, LOW);
-
-  //digitalWrite(fan, HIGH);
-  //delay(5000);
-  //digitalWrite(fan, LOW);
-
-  
-  //float voltage = rawValue * (5.0 / 1023.0);
   float setTemp = setpointVoltage * (5.0 / 1023.0) * 2 + 18; //Degrees celsius, set through pot. meter
 
   float voltageOutTmpSensor = rawVoltageTmpSensor * (5.0 / 1023.0);
   float tempSensorNow = (voltageOutTmpSensor - 0.424)/ 0.00625; //OBS! Be wary of bottow view of temp sensor, function from tmp sensor datasheet
-
-
 
   float tempDiff = tempSensorNow-setTemp;
   Serial.print("Tempdiff: ");
@@ -52,17 +39,13 @@ void loop() {
       analogWrite(fan, (255/5)*(tempDiff));
     }
     
-    //digitalWrite(fan, HIGH);
-
   } else {
     digitalWrite(redLed, LOW);
     digitalWrite(yellowLed, LOW);
     digitalWrite(greenLed, HIGH);
 
-    //digitalWrite(fan, LOW);
     analogWrite(fan, 0); //No speed
   }
-
 
   Serial.print(setTemp);
   Serial.println(" grader POTENTIOMETER");
